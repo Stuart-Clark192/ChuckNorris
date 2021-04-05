542346d7a91bcf8fce7c8689fc3c4594479c5427
Stuart Clark <>
2021-04-05 17:58:52 +0100

    Initial Project setup

---
+++ ChuckNorris/ChuckNorrisApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChuckNorrisApp.swift
+//  ChuckNorris
+//
+//  Created by Stuart on 05/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct ChuckNorrisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
